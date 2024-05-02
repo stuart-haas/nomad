81c2e5695080910ebfd96d3c424975051dbfddfb
Stuart Haas <stuart@visionbyscope.com>
2024-05-02 02:02:43 -0400

    improve go-test

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+go-test:
+	uuid=$$(docker run --rm -it -d -v ./:/app --workdir /app/go nomad-go go run main.go); \
+	docker exec -it --workdir /app/go $$uuid go test; \
+	docker container stop $$uuid
+
+python-test:
+	docker run --rm -it -v ./:/app --workdir /app/python/tests nomad-python pytest
+
 start-agent:
 	sudo nomad agent -dev -config config/server.hcl
 
--- python/src/main.py
+++ python/src/main.py
@@ -1,10 +1,12 @@
-import time
+import grpc
+from proto import api_pb2_grpc, api_pb2
 
 
 def main():
-    while True:
-        print("hello python")
-        time.sleep(3)
+    with grpc.insecure_channel("go:50051") as channel:
+        stub = api_pb2_grpc.InferenceServiceStub(channel)
+        res = stub.Inference(api_pb2.InferenceRequest(value=1))
+        print(f"Result: {res}")
 
 
 if __name__ == "__main__":
